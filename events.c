ffc5fd393baf467fbb8ca8c8dad90c78b26ca961
Dinis Machado <dimachad@student.42berlin.de>
2025-05-27 11:29:30 +0200

    Finalize Logic Zoom follow mouse

--- events.c
+++ events.c
@@ -6,81 +6,74 @@
 /*   By: dimachad <dimachad@student.42berlin.d>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 20:35:04 by dimachad          #+#    #+#             */
-/*   Updated: 2025/05/26 17:20:48 by dimachad         ###   ########.fr       */
+/*   Updated: 2025/05/27 11:19:18 by dimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int update_scale_image(t_frctl *fl) {
-  fl->x_width = 5.0 * fl->zoom;
-  fl->y_width = scale_y_px_to_math(fl);
-  if (fl->x_min < 0.0) {
-    fl->x_min = (fl->x_min + (fl->x_width / 2 / 2 * fl->x_offset)) / 2;
-    fl->y_min = (fl->y_min + (fl->y_width / 2 / 2 * fl->y_offset)) / 2;
-  } else {
-    fl->x_min = (fl->x_min + (fl->x_width / 2 / 2 * -fl->x_offset)) / 2;
-    fl->y_min = (fl->y_min + (fl->y_width / 2 / 2 * -fl->y_offset)) / 2;
-  }
-  printf("x_min %f, y_min %f || ", fl->x_min, fl->y_min);
-  printf("x_off %f, y_off %f || ", fl->x_offset, fl->y_offset);
-  printf("zoom %f\n", fl->zoom);
-  return (0);
+static void	zoom_and_recenter(int x, int y, t_frctl *fl, char zoom)
+{
+	fl->x_offset = fl->x_min + ((double)x / RES_WIDTH) * fl->x_width;
+	fl->y_offset = fl->y_min + ((double)y / RES_HIGHT) * fl->y_width;
+	if (zoom == '+')
+	{
+		fl->zoom *= 0.95;
+		fl->iterations_ratio += 0.5;
+	}
+	else
+	{
+		fl->zoom *= 1.05;
+		fl->iterations_ratio -= 0.5;
+	}
+	fl->x_width = 5.0 * fl->zoom;
+	fl->y_width = scale_y_px_to_math(fl);
+	fl->x_min = fl->x_offset - ((double)x / RES_WIDTH) * fl->x_width;
+	fl->y_min = fl->y_offset - ((double)y / RES_HIGHT) * fl->y_width;
+	printf("zoom :%f", fl->zoom);
+	printf(" || iterations ration :%f\n", fl->iterations_ratio);
 }
 
-// update zoom and calculate offset % center zoom by mouse position
-static void zoom_and_recenter(int x, int y, t_frctl *fl, char zoom) {
-  x = x - (RES_WIDTH / 2);
-  y = y - (RES_HIGHT / 2);
-  if (zoom == '-') {
-    fl->zoom *= 0.95;
-    x *= -1;
-    y *= -1;
-  } else
-    fl->zoom *= 1.05;
-  fl->x_offset = (fl->x_offset + (float)x / (float)RES_WIDTH) / 2;
-  fl->y_offset = (fl->y_offset + (float)y / (float)RES_HIGHT) / 2;
-  printf("x_off %f, y_off %f \n", fl->x_offset, fl->y_offset);
+int	render_zoom(t_frctl *fl, int quadratic)
+{
+	render_fractal(fl, quadratic);
+	fl->zoom_to_update = 0;
+	return (0);
 }
 
-int render_zoom(t_frctl *fl, int quadratic) {
-  update_scale_image(fl);
-  render_fractal(fl, quadratic);
-  fl->zoom_to_update = 0;
-  return (0);
+int	handle_mouse(int mouse_move, int x, int y, t_frctl *fl)
+{
+	if (mouse_move == 4)
+		zoom_and_recenter(x, y, fl, '+');
+	else if (mouse_move == 5)
+		zoom_and_recenter(x, y, fl, '-');
+	else
+		return (-1);
+	//	if (fl->zoom_to_update == 10)
+	//		return (render_zoom(fl, 1));
+	fl->zoom_to_update++;
+	return (0);
 }
 
-int handle_mouse(int mouse_move, int x, int y, t_frctl *fl) {
-  if (mouse_move == 5)
-    zoom_and_recenter(x, y, fl, '-');
-  else if (mouse_move == 4)
-    zoom_and_recenter(x, y, fl, '+');
-  else
-    return (-1);
-  if (fl->zoom_to_update == 10)
-    return (render_zoom(fl, 1));
-  else
-    fl->zoom_to_update++;
-  return (0);
+int	handle_keys(int keycode, t_frctl *fl)
+{
+	if (keycode == 65307)
+		return (close_all(fl));
+	else if (keycode == 65361)
+		fl->x_offset -= fl->x_width / 5;
+	else if (keycode == 65363)
+		fl->x_offset += fl->x_width / 5;
+	else if (keycode == 65362)
+		fl->y_offset -= fl->y_width / 5;
+	else if (keycode == 65364)
+		fl->y_offset += fl->y_width / 5;
+	init_scale_image(fl);
+	render_fractal(fl, 1);
+	return (-1);
 }
 
-int handle_keys(int keycode, t_frctl *fl) {
-  if (keycode == 65307)
-    return (close_all(fl));
-  else if (keycode == 65361)
-    fl->x_offset -= fl->x_width / 5;
-  else if (keycode == 65363)
-    fl->x_offset += fl->x_width / 5;
-  else if (keycode == 65362)
-    fl->y_offset -= fl->y_width / 5;
-  else if (keycode == 65364)
-    fl->y_offset += fl->y_width / 5;
-  init_scale_image(fl);
-  render_fractal(fl, 1);
-  return (-1);
-}
-
-int close_window(int keycode, t_frctl *fl) {
-  (void)keycode;
-  return (close_all(fl));
+int	close_window(int keycode, t_frctl *fl)
+{
+	(void)keycode;
+	return (close_all(fl));
 }
--- init.c
+++ init.c
@@ -6,52 +6,57 @@
 /*   By: dimachad <dimachad@student.42berlin.d>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 20:32:13 by dimachad          #+#    #+#             */
-/*   Updated: 2025/05/26 17:20:52 by dimachad         ###   ########.fr       */
+/*   Updated: 2025/05/27 08:52:49 by dimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-double scale_y_px_to_math(t_frctl *fl) {
-  return (fl->x_width * (double)RES_HIGHT / (double)RES_WIDTH);
+double	scale_y_px_to_math(t_frctl *fl)
+{
+	return (fl->x_width * (double)RES_HIGHT / (double)RES_WIDTH);
 }
 
-int init_mlx_structure(t_frctl *fl) {
-  fl->mlx = mlx_init();
-  if (!fl->mlx)
-    p_err("Err: Failed to initiate MLX", fl);
-  fl->mlx_win = mlx_new_window(fl->mlx, RES_WIDTH, RES_HIGHT, "Fract_ol");
-  if (!fl->mlx_win)
-    p_err("Err: Failed to create window", fl);
-  fl->img.img = mlx_new_image(fl->mlx, RES_WIDTH, RES_HIGHT);
-  if (!fl->img.img)
-    p_err("Err: Failed to create image", fl);
-  fl->img.addr = mlx_get_data_addr(fl->img.img, &fl->img.bpp, &fl->img.line_len,
-                                   &fl->img.endian);
-  return (0);
+int	init_mlx_structure(t_frctl *fl)
+{
+	fl->mlx = mlx_init();
+	if (!fl->mlx)
+		p_err("Err: Failed to initiate MLX", fl);
+	fl->mlx_win = mlx_new_window(fl->mlx, RES_WIDTH, RES_HIGHT, "Fract_ol");
+	if (!fl->mlx_win)
+		p_err("Err: Failed to create window", fl);
+	fl->img.img = mlx_new_image(fl->mlx, RES_WIDTH, RES_HIGHT);
+	if (!fl->img.img)
+		p_err("Err: Failed to create image", fl);
+	fl->img.addr = mlx_get_data_addr(fl->img.img, &fl->img.bpp,
+			&fl->img.line_len, &fl->img.endian);
+	return (0);
 }
 
-int init_scale_image(t_frctl *fl) {
-  fl->x_width = 5.0 * fl->zoom;
-  fl->x_min = (fl->x_width / 2 * -1);
-  fl->y_width = scale_y_px_to_math(fl);
-  fl->y_min = (fl->y_width / 2 * -1);
-  // printf("x_min %f, y_min %f || ", fl->x_min, fl->y_min);
-  // printf("x_off %f, y_off %f || ", fl->x_offset, fl->y_offset);
-  // printf("zoom %f\n", fl->zoom);
-  return (0);
+int	init_scale_image(t_frctl *fl)
+{
+	fl->x_width = 5.0 * fl->zoom;
+	fl->x_min = (fl->x_width / 2 * -1);
+	fl->y_width = scale_y_px_to_math(fl);
+	fl->y_min = (fl->y_width / 2 * -1);
+	// printf("x_min %f, y_min %f || ", fl->x_min, fl->y_min);
+	// printf("x_off %f, y_off %f || ", fl->x_offset, fl->y_offset);
+	// printf("zoom %f\n", fl->zoom);
+	return (0);
 }
 
-static int render_zoom_full(t_frctl *fl) {
-  if (fl->zoom_to_update)
-    render_zoom(fl, 0);
-  return (0);
+static int	render_zoom_full(t_frctl *fl)
+{
+	if (fl->zoom_to_update)
+		render_zoom(fl, 0);
+	return (0);
 }
 
-int init_events(t_frctl *fl) {
-  mlx_mouse_hook(fl->mlx_win, handle_mouse, fl);
-  mlx_hook(fl->mlx_win, KeyPress, KeyPressMask, handle_keys, fl);
-  mlx_hook(fl->mlx_win, DestroyNotify, StructureNotifyMask, close_all, fl);
-  mlx_loop_hook(fl->mlx, render_zoom_full, fl);
-  return (0);
+int	init_events(t_frctl *fl)
+{
+	mlx_mouse_hook(fl->mlx_win, handle_mouse, fl);
+	mlx_hook(fl->mlx_win, KeyPress, KeyPressMask, handle_keys, fl);
+	mlx_hook(fl->mlx_win, DestroyNotify, StructureNotifyMask, close_all, fl);
+	mlx_loop_hook(fl->mlx, render_zoom_full, fl);
+	return (0);
 }
